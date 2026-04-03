aa297ef5bb566882e288d91ba22c5db92bd624df
comp340 <comp340@cs340lnx114.GCC.edu>
2026-04-03 12:07:14 -0400

    implemented task one without random thinking and eating times

--- task1.c
+++ task1.c
@@ -8,46 +8,29 @@
 
 //function for the philosopher to pickup the chopsticks
 void pickup_chopsticks(int number){
-    //lock
-    //check to see if semaphore value > 0. if not, release lock and loop again. if so, ea
-    //wait for philosopher semaphore. when get it,  (could be -2, could be 0)
-    //grab lock
-    //check left and right philosopher to see if they're eating, if not, grab and eat
-    //release lock
-    
-    
-    //Q: does it matter if I'm changing between hungry and thinking while reading this? do I       need to lock those too
-    
-  //loop until can eat:
-  
-    //lock (so they don't change to eating while I'm picking up)
-    //check left and right; if neither is eating, change status to eating and release lock.
-    //if one is eating, release lock and loop again
-    
-    //Q: why do I need a semaphore? 
-    //So I don't take up time slice
-    
-    //bool eating = false;
-    //while(!eating){
-
-      //wait for both chopsticks to be set down
-      sem_wait(&sem_vars[number]);
-      sem_wait(&sem_vars[number]);
-      pthread_mutex_lock(&mutex_lock);
+    state[number] = HUNGRY;
+    printf("                thread %d\n",*p_num); //hungry
+    bool eating = false;
+    while(!eating){
+      //printf("thread %d waiting\n", number);
+      sem_wait(&sem_vars[number]); //one signal from either side should allow this code to progress
+      //still need to check
       
-      //TODO: don't need this if statement if it works correctly
+      //once the sem value is greater than 0
+      pthread_mutex_lock(&mutex_lock);
       if(state[(number+1)%NUMBER]!= EATING && state[(number - 1 + NUMBER) % NUMBER] != EATING){
-        printf("Thread %d eating\n", number);
         state[number] = EATING;
-        
-        
-        //eating = true;
+        sem_trywait(&sem_vars[(number+1)%NUMBER]); //decrement values on either side so they know to wait
+        sem_trywait(&sem_vars[(number - 1 + NUMBER) % NUMBER]);
+        eating = true;
       }else{
-        printf("said that one of the neighbors was eating, error\n");
+        //printf("said that one of the neighbors was eating, error\n");
+        
+        //just loop again, wait for someone to signal and check both again
       }
 
       pthread_mutex_unlock(&mutex_lock);
-    //} 
+    }
   
 }
 
@@ -55,7 +38,13 @@ void pickup_chopsticks(int number){
 
 //function for the philosopher to return the chopsticks
 void return_chopsticks(int number){
-
+  pthread_mutex_lock(&mutex_lock);
+  state[number] = THINKING;
+  sem_post(&sem_vars[(number+1)%NUMBER]); //signal the left and the right 
+  sem_post(&sem_vars[(number - 1 + NUMBER) % NUMBER]);
+  sem_post(&sem_vars[number]);
+  pthread_mutex_unlock(&mutex_lock);
+  //signal both
 }
 
 
@@ -68,34 +57,29 @@ void *philosopher(void* param){
   for(int i = 0; i < 5; i++){  //complete cycle 5 times
   
     //begin in the thinking state
-      //think for a random amount of time (bounded by what? don't want infinite time)
+    printf("thread %d\n",*p_num);
     
-    
-    //TODO: Change
-    int lower = 1, upper = 3;
-    int num = (rand() % (upper - lower + 1)) + lower;
-    sleep(num);
+    //TODO: think for a random amount of time 
+    sleep(2);
     
     //change state to hungry, try to pick up the two chopsticks next to it
-    state[p_num] = HUNGRY;
-    
-    //try to eat
     pickup_chopsticks(*p_num);
     
     //now eating
+    printf("                                 thread %d\n",*p_num); //eating
     
-    //check if they're eating or not?
-      //check (p+1)%N, and (p-1)%N at the same time, wait until they're free. How use locks for         this?
+    
+    //TODO: eat for a random amount of time
+    sleep(2);
+    
+    //finish eating
+    return_chopsticks(*p_num);
+    
     
     //when successful, change state to eating, eat for a random amount of time, then release.
     
     //end this loop, go back to thinking at the top.
-    state[p_num] = THINKING;
-    
-    //tell the philosophers on either side that you've put down the chopsticks
-    signal((number - 1 + NUMBER) % NUMBER)
-    sem_post(&sem_vars[number - 1 + NUMBER) % NUMBER]); //left one
-    sem_post(&sem_vars[number+1) % NUMBER]); //right phil one
+
     
     
   }
@@ -110,18 +94,27 @@ int main(int argc, char *argv[]){
 
   pthread_mutex_init(&mutex_lock, NULL);
   
+  printf("THINKING        HUNGRY           EATING\n");
+  
+  pthread_t threads[NUMBER]; // thread id 
+  
   //initialize all thread states as thinking?
   for(int i = 0; i < NUMBER; i++){
     //set state to THINKING
     state[i] = THINKING;
-    sem_init(&sem_vars[i], 0, 2); //each semaphore starts with two chopsticks next to them
+    sem_init(&sem_vars[i], 0, 1); //each semaphore starts with two chopsticks next to them
     thread_id[i] = i;
-    //TODO: create thread with philosopher number == i
+    pthread_create(&threads[i], NULL, philosopher, (void*)(&thread_id[i]));
   }
   
-  //TODO: join threads to wait for them
+
   
-  philosopher(&thread_id[0]);
+  //philosopher(&thread_id[0]);
+  
+  //TODO: join threads to wait for them
+  for(int i=0; i < NUMBER; i++){
+   pthread_join(threads[i],NULL); //wait for threads to exit
+   }
   
   
   return 0;
